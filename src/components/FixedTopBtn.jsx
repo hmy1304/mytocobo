c7f810801a3995c4a1ab666243661be43d660c92
unknown <hmy1304@kbu.ac.kr>
2026-02-26 17:26:55 +0900

    update

--- src/components/FixedTopBtn.jsx
+++ src/components/FixedTopBtn.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import "./styles/FixedTopBtn.scss"
+import { useSmoothScroll } from '../hooks/useSmoothScroll'
 
 const FixedTopBtn = () => {
+  const scrollTo = useSmoothScroll()
+
   return (
-    <div>FixedTopBtn</div>
+    <div className='FixedTopbtn'>
+      <button
+      className='top-btn'
+      onClick={(e)=>{
+        e.preventDefault
+        scrollTo('hero')
+      }}>
+
+      </button>
+      <a href="#" className='talk-btn'>
+        TCB
+      </a>
+    </div>
   )
 }
 
